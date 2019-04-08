707f010ffd1efbb3ec99c1a8cfc438dac50bdf09
Odoia <tiagohenriqueodoia@gmail.com>
2019-04-08 09:20:05 -0300

    develop Refactor a class and test
    - remove not used variables and organize code
    - extract many instances for a let
    
    Signed-off-by: Odoia <tiagohenriqueodoia@gmail.com>

--- stant_web_selection/lecture.rb
+++ stant_web_selection/lecture.rb
@@ -4,73 +4,24 @@ class Lecture
     $left_speeches = get_speeches(get_txt) 
   end
 
-  def organize_lectures
-    track_a_morning = []
-    track_b_morning = []
-    track_a_afternoon = []
-    track_b_afternoon = []
+  def get_txt
+    path  = File.expand_path("proposals.txt", 'speeches/')
+    file  = File.open(path)
+    txt_array = []
 
-    turn_a_start = 540
-    turn_b_start = 780
-    duration_turn_morning = 180
-    duration_turn_afternoon = 240
-
-    track_a_morning   = organize_speeches(track_a_morning, duration_turn_morning)
-    track_b_morning   = organize_speeches(track_b_morning, duration_turn_morning)
-    track_a_afternoon = organize_speeches(track_a_afternoon, duration_turn_afternoon)
-    track_b_afternoon = organize_speeches(track_b_afternoon, duration_turn_afternoon)
-   
-
-    puts 'Track A'
-    get_lunch(print_track(track_a_morning, turn_a_start))
-    get_networking(print_track(track_a_afternoon, turn_b_start))
-    puts 'Track B'
-    get_lunch(print_track(track_b_morning, turn_a_start))
-    get_networking(print_track(track_b_afternoon, turn_b_start))
-  end
-
-  def print_track(track, turn )
-    current_time_in_minutes = turn
-    track.each do |a|
-      puts "#{conference_hour(current_time_in_minutes)} -> #{a[0]}"  
-      current_time_in_minutes += a[1]
-    end 
-    current_time_in_minutes
-  end
-
-  def get_networking(total_minutes)
-    puts "#{conference_hour(total_minutes)} -> Networking Time !!"
-  end
-
-  def get_lunch(total_minutes)
-    puts "#{conference_hour(total_minutes)} -> Lunch Time !!"
-  end
-
-  def organize_speeches( organized_speeches, duration)
-    total_minutes = 0
-    current_speeches = $left_speeches
-    $left_speeches = []
-    current_speeches.each do |l|
-      conditional = total_minutes + l[1] <= duration
-      if duration == 180
-        conditional = total_minutes + l[1] <= duration && l[1] != 5
-      end
-      if conditional
-        organized_speeches.push(l)
-        total_minutes += l[1] 
-      else
-        $left_speeches.push(l)
-      end
+    while ! file.eof?
+      txt_array << file.gets.chomp
     end
     
-    organized_speeches
+    file.close
+    txt_array
   end
 
   def get_speeches(txt_array)
     speeches_array = []
-    i = 0
+    
     txt_array.each do |lecture|
-      lecture_time =  get_lecture_time(lecture)
+      lecture_time = get_lecture_time(lecture)
       lecture_time.cycle(1) do |minutes| 
         speeches_array.push([lecture, minutes.to_i])   
       end        
@@ -82,28 +33,71 @@ class Lecture
     lecture.scan(/[0-9][0-9]/) == [] ? ["05"] : lecture.scan(/[0-9][0-9]/)
   end
 
-  def get_txt
+  def organize_lectures
+    turn_a_start = 540
+    turn_b_start = 780
+    duration_turn_morning   = 180
+    duration_turn_afternoon = 240
 
-    path  = File.expand_path("proposals.txt", 'speeches/')
-    file = File.open(path)
-    txt_array = []
+    track_a_morning   = organize_speeches(duration_turn_morning)
+    track_b_morning   = organize_speeches(duration_turn_morning)
+    track_a_afternoon = organize_speeches(duration_turn_afternoon)
+    track_b_afternoon = organize_speeches(duration_turn_afternoon)
+   
 
-    while ! file.eof?
-      txt_array << file.gets.chomp
+    puts 'Track A'
+    get_lunch(print_track(track_a_morning, turn_a_start))
+    get_networking(print_track(track_a_afternoon, turn_b_start))
+    puts 'Track B'
+    get_lunch(print_track(track_b_morning, turn_a_start))
+    get_networking(print_track(track_b_afternoon, turn_b_start))
+  end
+
+  def organize_speeches(duration)
+    speeches_to_organize = []
+    total_minutes        = 0
+    current_speeches     = $left_speeches
+    $left_speeches       = []
+
+    current_speeches.each do |l|
+      minutes_after_speeche = total_minutes + l[1]
+      conditional = minutes_after_speeche <= duration   
+
+      if duration == 180
+        conditional = conditional && l[1] != 5
+      end
+
+      if conditional
+        speeches_to_organize.push(l)
+        total_minutes += l[1] 
+      else
+        $left_speeches.push(l)
+      end
     end
     
-    file.close
-    txt_array
+    speeches_to_organize
+  end
+
+  def print_track(track, turn)
+    current_time_in_minutes = turn
+    track.each do |a|
+      puts "#{conference_hour(current_time_in_minutes)} -> #{a[0]}"  
+      current_time_in_minutes += a[1]
+    end 
+    current_time_in_minutes
+  end
+
+  def get_lunch(total_minutes)
+    puts "#{conference_hour(total_minutes)} -> Lunch Time !!"
+  end
+
+  def get_networking(total_minutes)
+    puts "#{conference_hour(total_minutes)} -> Networking Time !!"
   end
 
   def conference_hour(time)
-    if time/60 == 0
-      hour = time/60
-      minute = 00
-    else
-      hour = time/60
-      minute = time.remainder 60
-    end
+    hour   = time/60
+    minute = time.remainder 60
 
     "#{hour.to_s.rjust(2, '0')}:#{minute.to_s.rjust(2, '0')}" 
   end
--- stant_web_selection/spec/conference_spec.rb
+++ stant_web_selection/spec/conference_spec.rb
@@ -2,9 +2,17 @@ require 'spec_helper'
 require './lecture'
 
 describe 'Organize lectures from a conference by txt file' do
+
+  let(:lecture) { Lecture.new }
+  let(:execute_actions) {}
+
+  before do
+    lecture
+    execute_actions
+  end
+
   context 'when have a file with 19 lines' do
-    
-    lecture = Lecture.new
+
     it 'Show the correct size of array' do 
       expect(lecture.get_txt.size).to eq 19
     end 
@@ -12,23 +20,20 @@ describe 'Organize lectures from a conference by txt file' do
 
   context 'when need to separate the time of each lecture' do
     context 'when the lecture has the time' do
+   
+      title = 'Erros comuns em Ruby 45min'
       
-      string = 'Erros comuns em Ruby 45min'
-      lecture = Lecture.new
-
       it 'show the minutes lecture' do
-        expect(lecture.get_lecture_time(string)).to eq ["45"]
+        expect(lecture.get_lecture_time(title)).to eq ["45"]
       end
     end
 
     context "when the lecture don't have the time" do
       context 'use a default value' do
-
-        string = 'Rails para usuários de Django lightning'
-        lecture = Lecture.new
+        title = 'Rails para usuários de Django lightning'
 
         it 'show the default minutes(5min) to lecture' do
-          expect(lecture.get_lecture_time(string)).to eq ["05"]
+          expect(lecture.get_lecture_time(title)).to eq ["05"]
         end
       end
     end
@@ -36,23 +41,19 @@ describe 'Organize lectures from a conference by txt file' do
 
   context 'when need to populate an array with lecture and time' do
     context 'when have minutes in the array' do
-      
-      lecture = Lecture.new
-      string_array = ['Erros comuns em Ruby 45min']
+      speeche = ['Erros comuns em Ruby 45min']
 
       it 'show correct string array and correct int minutes' do
-       expect(lecture.get_speeches(string_array)).to eq [['Erros comuns em Ruby 45min', 45]]
+       expect(lecture.get_speeches(speeche)).to eq [['Erros comuns em Ruby 45min', 45]]
       
       end
     end
 
     context "when don't have minutes in the array  " do
-      
-      lecture = Lecture.new
-      string_array = ['Rails para usuários de Django lightning']
+      speeche = ['Rails para usuários de Django lightning']
 
       it 'show correct string array and correct int minutes to default value' do
-        expect(lecture.get_speeches(string_array)).to eq [['Rails para usuários de Django lightning', 05]]
+        expect(lecture.get_speeches(speeche)).to eq [['Rails para usuários de Django lightning', 05]]
       
       end
     end
@@ -60,14 +61,11 @@ describe 'Organize lectures from a conference by txt file' do
 
   context 'when need organize speeches by turns' do
     context 'when need a morning turn 180 minutes  ' do
-      lecture = Lecture.new
-      received_list = lecture.get_speeches(lecture.get_txt)
-      received_list_copy = received_list.dup
-      empty_array = []
       a = 0
-      result = lecture.organize_speeches(empty_array, 180)
-      result.each do |o|
-        a += o[1]
+      let(:execute_actions) do
+        lecture.organize_speeches(180).each do |o|
+          a += o[1]
+        end
       end
       
       it 'show a list of 180 minutes track' do
@@ -76,16 +74,13 @@ describe 'Organize lectures from a conference by txt file' do
     end
 
     context 'when need a afternoon turn 240 minutes  ' do
-      lecture = Lecture.new
-      received_list = lecture.get_speeches(lecture.get_txt)
-      received_list_copy = received_list.dup
-      empty_array = []
       a = 0
-      result = lecture.organize_speeches(empty_array, 240)
-      result.each do |o|
-        a += o[1]
+      let(:execute_actions) do
+        lecture.organize_speeches(240).each do |o|
+          a += o[1]
+        end
       end
-      
+
       it 'show a list between 180 and 240 minutes track' do
         expect(a).to satisfy { |x| x >= 180 && x <= 240  }
       end
